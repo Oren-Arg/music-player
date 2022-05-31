9da6024309b935f06e8bd11f4de54ff298b13d6f
Oren Argaman <oren1219@gmail.com>
2022-05-31 17:31:05 +0300

    updated files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import * as React from "react";
+
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
