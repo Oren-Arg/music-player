c23cf01b8bd165f2476b884484ba0634b3b5df54
Oren Argaman <oren1219@gmail.com>
2022-06-05 13:33:39 +0300

    import from spotify api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import * as React from "react";
 
-import Header from "./components/Header";
-import Main from "./components/Main";
+import Layout from "./components/Layout";
+
 function App() {
   return (
     <>
-      <Header />
-      <Main />
+      <Layout />
     </>
   );
 }
